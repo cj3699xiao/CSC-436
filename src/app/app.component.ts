f1bcb60e41d93c12b5a3e8912316500e2779fde3
cj3699xiao <354123302@qq.com>
2020-10-09 23:46:24 -0500

    Class Demo finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Yuchen Xiao';
+  title:string = 'Calculator';
+
+  lhs:number = 0;
+  rhs:number = 0;
+  result:number = 0;
+  operator: string = '+';
+
+
+  recalc(): void {
+    // console.log(this.operator);
+    const op = this.operator.charAt(0);
+    // console.log(op);
+    if(op === '+') {
+      console.log('Add');
+      this.result = this.lhs + this.rhs;
+    } else if(op === '-') {
+      console.log('Minus');
+      this.result = this.lhs - this.rhs;
+    } else if(op === '*') {
+      console.log('Times');
+      this.result = this.lhs * this.rhs;
+    } else {
+      console.log('Divide');
+      this.result = this.lhs / this.rhs;
+    }
+  }
 }
+
